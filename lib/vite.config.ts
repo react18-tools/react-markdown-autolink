c1e00cc7d2f48f97135d8f898da89e3e9c8ed008
Mayank <mayank.srmu@gmail.com>
2024-07-12 17:00:22 +0530

    Build with vite

--- lib/vite.config.ts
+++ lib/vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from "vitest/config";
-import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [tsconfigPaths()],
+  build: {
+    lib: {
+      formats: ["cjs", "es"],
+      entry: "./src/index.ts",
+      fileName: format => `index.${format === "es" ? "m" : ""}js`,
+    },
+  },
   test: {
     environment: "jsdom",
     globals: true,
