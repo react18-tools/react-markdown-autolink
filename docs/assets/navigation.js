4a0914db20bd5b87e1685de283cb7f404e878bb1
Mayank <mayank.srmu@gmail.com>
2024-07-12 17:10:39 +0530

    Fix documentation

--- docs/assets/navigation.js
+++ docs/assets/navigation.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAAA4uOBQApu0wNAgAAAA=="
-=======
 window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAACouuVipJrShRslLKzEtJrVDSUSpILMlQslLKzU8pzUkt1gcL62WU5OYo6ShlZ+alKFkZ6SglZ2TmpBSl5ilZRcMNSCwtyffJzMv2TUGYklaal1ySmZ8HMwehBtVIM5Pa2FodVLfolaQWl+BwUDxIDs1VtbEAsbTyR80AAAA="
->>>>>>> d7239f9 (Fix documentation)
--- docs/assets/search.js
+++ docs/assets/search.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-window.searchData = "data:application/octet-stream;base64,H4sIAAAAAAAAAz2MQQqDMBRE7zLr4MKuzA16ATfioiQjfDD/S0xtQby7pEp384bH25Hts8IPo4No5Bd+x8a8iik82ubRdHCYhHOsGvSVCIdgKVELHKKF92+Ot9YzFMv/5sZcGJ9Xu16LLJxFWek4TiJTfzuBAAAA";
-=======
 window.searchData = "data:application/octet-stream;base64,H4sIAAAAAAAACo2S3WrEIBCF3+VcD9kkLWXXNyi0t72RsIToUlmjSzTbgvjuJSYbW5r+3MmZOZ/jGQMG++bAeMBZGQFWE0zbSzAoI+R74aXzIIyDBkNvxail26XScSoVr77XIHS6dU46MCDSD6xtzK+Eh/sV0Y7ePilzfhYr5zSazitrbqTcsgElXNpBGr9OExtaTizgKgenrAFDXdwVBxBOSmoxJTMPQOhs309+grDdmI7N0vYiO2+HqXnu3pUgXtKh2Jf7piF+8yY9CTdEVpKvAvFqw1d981VffDWI1xu++ptvVtLTr3LwUjzOEXCeEtbKnHsBCjgu2axLDKjBQow5CRbipzCm2nTVsutMqDKh+j9h+XkZU2ZM+TemIVzURWplJBhvYvwATWnlPOoCAAA=";
->>>>>>> d7239f9 (Fix documentation)
