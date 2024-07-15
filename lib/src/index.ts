9bb33716fa6689e653ad89082454615c4fe9a59f
Mayank <mayank.srmu@gmail.com>
2024-07-15 10:21:55 +0530

    check codeql analysis

--- lib/src/index.ts
+++ lib/src/index.ts
@@ -1,5 +1,7 @@
 export const autoLinkMd = (str: string) =>
-  str.replace(
-    /(ftp|https?):\/\/[\w_-]+(\.[\w_-]+)+[\w@?^=%&/~+#-.:,]*[\w@?^=%&/~+#-]|[\w._%+-]+@[\w.-]+\.[a-zA-Z]{2,}/g,
-    match => `<${match}>`,
-  );
+  str
+    .replace(
+      /(ftp|https?):\/\/[\w_-]+(\.[\w_-]+)+[\w@?^=%&/~+#-.:,]*[\w@?^=%&/~+#-]/g,
+      match => `<${match}>`,
+    )
+    .replace(/[\w._%+-]+@[\w.-]+\.[a-zA-Z]{2,}/g, match => `<${match}>`);
