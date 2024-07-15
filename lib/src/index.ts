5ff99b1c27309c3f6ea3427e08b9c2e83e80fad4
Mayank <mayank.srmu@gmail.com>
2024-07-15 10:28:47 +0530

    fix unexpected range.

--- lib/src/index.ts
+++ lib/src/index.ts
@@ -1,7 +1,7 @@
 export const autoLinkMd = (str: string) =>
   str
     .replace(
-      /(ftp|https?):\/\/[\w_-]+(\.[\w_-]+)+[\w@?^=%&/~+#-.:,]*[\w@?^=%&/~+#-]/g,
+      /(ftp|https?):\/\/[\w_-]+(\.[\w_-]+)+[\w@?^=%&/~+#.:,-]*[\w@?^=%&/~+#-]/g,
       match => `<${match}>`,
     )
     .replace(/[\w._%+-]+@[\w.-]+\.[a-zA-Z]{2,}/g, match => `<${match}>`);
